d848d679793a3b30dc831b49e25aa1146afd942d
vikneswaran <vickyrahul005@gmail.com>
2018-02-10 23:09:02 +0530

    Implemented the text validation

--- Hangman/main.cpp
+++ Hangman/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void PrintMessage(string message, bool printTop = true, bool printBottom = true) 
+void PrintMessage(string message, bool printTop = true, bool printBottom = true)
 {
 	if (printTop) {
 		cout << "+---------------------------------+" << endl;
@@ -20,18 +20,18 @@ void PrintMessage(string message, bool printTop = true, bool printBottom = true)
 		}
 		front = !front;
 	}
-	cout << "|" << message.c_str() << "|"<<endl;
+	cout << "|" << message.c_str() << "|" << endl;
 	if (printBottom)
 	{
 		cout << "+---------------------------------+" << endl;
 	}
 }
 
-void DrawHangman(int guessCount=0)
+void DrawHangman(int guessCount = 0)
 {
 	if (guessCount >= 1)
 		PrintMessage("|", false, false);
-	if(guessCount >= 2)
+	if (guessCount >= 2)
 		PrintMessage("|", false, false);
 	if (guessCount >= 3)
 		PrintMessage("O", false, false);
@@ -49,10 +49,57 @@ void DrawHangman(int guessCount=0)
 		PrintMessage("/ \\", false, false);
 }
 
+void PrintLetters(string input, char from, char to)
+{
+	string alphabets;
+	for (char i = from; i <= to; i++)
+	{
+		if (input.find(i) == string::npos)
+		{
+			alphabets += i;
+			alphabets += " ";
+		}
+		else
+			alphabets += "  ";
+	}
+	PrintMessage(alphabets, false, false);
+}
+
+void PrintRemainingLetters(string taken)
+{
+	PrintMessage("AVAILABLE LETTERS");
+	PrintLetters(taken, 'A', 'M');
+	PrintLetters(taken, 'N', 'Z');
+}
+
+bool PrintWordAndCheckWin(string word, string guess)
+{
+	bool won = true;
+	string chkWord;
+	for (int i = 0; i < word.length(); i++)
+	{
+		if (guess.find(word[i]) == string::npos)
+		{
+			chkWord += "_ ";
+		}
+		else
+		{
+			chkWord += word[i];
+			chkWord += " ";
+		}
+	}
+	PrintMessage(chkWord, false, true);
+	return won;
+}
+
 int main()
 {
+	string guesses = "SOME";
 	PrintMessage("HANGMAN");
 	DrawHangman(9);
+	PrintRemainingLetters(guesses);
+	PrintMessage("Guess the word");
+	PrintWordAndCheckWin("SOMETHING", guesses);
 	getchar();
 	return 0;
 }
